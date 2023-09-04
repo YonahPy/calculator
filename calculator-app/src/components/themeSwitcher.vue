3ce2d7712e9da3c581499be2312ac9769575fe48
YonahPy <jrjonas487@gmail.com>
2023-09-04 11:34:36 -0300

    Resolver bug do themeSwitcher

--- calculator-app/src/components/themeSwitcher.vue
+++ calculator-app/src/components/themeSwitcher.vue
@@ -6,39 +6,46 @@
             <span>3</span>
         </div>
         <div class="button-themes">
-            <span class="circle" :class="{'circle-animation-1': isAnimating1, 'circle-animation-2': isAnimating2, 'circle-animation-3': isAnimating3}" @click="toggleAnimation">
-                
+            <span class="circle" @click="toggleAnimation" :class="{'theme-one': theme1, 'theme-two': theme2, 'theme-three': theme3}" id="circle">
+               
             </span>
         </div>
     </div>
 </template>
 
 <script>
+
+
 export default{
 
     data(){
         return{
-            isAnimating1: false,
-            isAnimating2: false,
-            isAnimating3: false,
+            initialState: true,
+            theme1: true,
+            theme2:false,
+            theme3:false
         }
     },
     methods:{
-        toggleAnimation() {
-            if (!this.isAnimating1 && !this.isAnimating2 && !this.isAnimating3) {
-            this.isAnimating1 = true;
-            } else if (this.isAnimating1) {
-            this.isAnimating1 = false;
-            this.isAnimating2 = true;
-            } else if (this.isAnimating2) {
-            this.isAnimating2 = false;
-            this.isAnimating3 = true;
-            } else {
-            this.isAnimating3 = false;
-            this.isAnimating1 = true;
-            }
-            }
-}}
+      toggleAnimation() {
+        if (this.theme1) {
+          this.theme1 = false;
+          this.theme2 = true;
+          this.theme3 = false;
+          
+        } else if (this.theme2) {
+          this.theme1 = false;
+          this.theme2 = false;
+          this.theme3 = true;
+          
+        } else if (this.theme3) {
+          this.theme1 = true;
+          this.theme2 = false;
+          this.theme3 = false;
+          
+        }
+      }
+    }}
 
 </script>
 
@@ -47,6 +54,7 @@ export default{
     display: flex;
     flex-direction: column;
     align-items: center;
+    width: 70px;
 
 }
 .numbers{
@@ -56,7 +64,7 @@ export default{
 }
 .button-themes{
     background-color: hsl(224, 36%, 15%);
-    width: 70px;
+    width: 100%;
     height: 30px;
     border-radius: 50px;
     display: flex;
@@ -66,44 +74,60 @@ export default{
 }
 .circle{
     background-color: hsl(6, 63%, 50%);
-    width: 20px;
+    width: 30%;
     height: 20px;
     border-radius: 50%;
     margin-left: 5px;
     
 }
-.circle-animation{
-    animation: circle 1s linear;
+
+.theme-two{
+  animation: themeTwo 1s ease-in-out forwards;
 }
+.theme-three{
+  animation: themeThree 1s ease-in-out forwards;
+}
+.theme-one{
+  animation: initialState;
+  animation: themeOne 1s ease-in-out;
+}
+
+
 p{
     font-size: 14px;
     margin-right: 10px;
 }
 
-@keyframes circle-1 {
-  0% {
-    transform: translateX(0%);
-  }
-  100% {
+@keyframes initialState{
+  to{
     transform: translateX(0%);
   }
 }
 
-@keyframes circle-2 {
-  0% {
+@keyframes themeTwo{
+  0%{
     transform: translateX(0%);
   }
-  100% {
-    transform: translateX(50%);
+  100%{
+    transform: translateX(90%);
+  }
+  
+}
+@keyframes themeThree{
+  0%{
+    transform: translateX(90%);
+    
+  }
+  100%{
+    transform: translateX(190%);
   }
 }
-
-@keyframes circle-3 {
-  0% {
-    transform: translateX(0%);
+@keyframes themeOne{
+  0%{
+    transform: translateX(190%);
   }
-  100% {
-    transform: translateX(100%);
+  100%{
+    transform: translateX(0%);
   }
 }
 </style>
